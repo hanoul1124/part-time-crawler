872a2a667d73dc6b4e7a1749635f8caa56de038a
DoHyang Kim <hanoul1124@gmail.com>
2019-09-16 10:42:42 +0900

    last commitment for partime job crawler project with NAVER SENS

---
+++ app.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import json
+import requests
+from selenium import webdriver
+from selenium.common.exceptions import *
+
+options = webdriver.ChromeOptions()
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+driver = webdriver.Chrome(executable_path='***/part-time-job/chromedriver', chrome_options=options)
+
+secrets = json.load(open('secrets.json'))
+albamon_url = secrets['URL']
+
+driver.implicitly_wait(3)
+driver.get(albamon_url)
+
+new_alba_list = []
+alba_list = driver.find_elements_by_css_selector('div.gListWrap > table > tbody > tr')
+
+try:
+    for index, alba in enumerate(alba_list):
+        if '분전' in alba.find_element_by_css_selector('td.recently > em').text:
+            new_alba_list.append(int(index))
+
+    for index in new_alba_list:
+        new_alba = driver.find_elements_by_css_selector('div.gListWrap > table > tbody > tr')[index]
+        name = new_alba.find_element_by_css_selector('p.cName > a').text
+        discription = new_alba.find_element_by_css_selector('p.cTit > a').text
+        time = new_alba.find_element_by_css_selector('td:nth-child(4)').text
+        link = new_alba.find_element_by_css_selector('p.cName > a').get_attribute('href')
+        alba_text = f'{name}\n{discription}/{time}'
+        alba_link = f'{link}'
+
+        message = f'<도향봇 신규 알바>\n' + alba_text
+
+        service_id = secrets["SENS_SERVICE_ID"]
+        send_url = f'https://api-sens.ncloud.com/v1/sms/services/{service_id}/messages'
+        headers = {
+            "Content-Type": "application/json; charset=utf-8",
+            "X-NCP-auth-key": secrets["X-NCP-AUTH-KEY"],
+            "X-NCP-service-secret": secrets["X-NCP-SERVICE-SECRET"]
+        }
+        body = {
+            "type": "SMS",
+            "from": secrets["FROM_PHONE_NUMBER"],
+            "to": [
+                secrets["TO_PHONE_NUMBER"]
+            ],
+            "content": message
+        }
+        body2 = {
+            "type": "SMS",
+            "from": secrets["FROM_PHONE_NUMBER"],
+            "to": [
+                secrets["TO_PHONE_NUMBER"]
+            ],
+            "content": alba_link
+        }
+
+        res1 = requests.post(send_url, headers=headers, data=json.dumps(body))
+        res2 = requests.post(send_url, headers=headers, data=json.dumps(body))
+
+except NoSuchElementException:
+    pass
+
+
+driver.implicitly_wait(20)
+
+driver.close()
+
+driver.implicitly_wait(10)
+
+driver.quit()
+
